80c895045c7f16c4d4e7ae734ffdfa06f4988269
ivankp <ivan.pogrebnyak@gmail.com>
2017-05-17 15:36:48 +0200

    framework variables

--- Root/NNLOPSWeights.cxx
+++ Root/NNLOPSWeights.cxx
@@ -7,6 +7,7 @@
 #include <TLorentzVector.h>
 
 #include <HGamAnalysisFramework/HgammaIncludes.h>
+#include <HGamAnalysisFramework/HGamVariables.h>
 
 namespace detail {
   template <typename S, typename T>
@@ -30,52 +31,33 @@ ClassImp(NNLOPSWeights)
 using std::cout;
 using std::endl;
 
-class hist_bundle {
-  static constexpr size_t bundle_size = 1;
-  static const std::array<std::string,bundle_size> wnames;
-  static std::vector<const hist_bundle*> all;
-
-  std::array<TH1D*,bundle_size> hh;
-
-public:
-  static std::array<double,bundle_size> weights;
-
-  template <size_t N>
-  hist_bundle(const char* name, const double(&bins)[N]) {
-    all.push_back(this);
-    for (size_t i=0; i<bundle_size; ++i)
-      hh[i] = new TH1D( cat(name,':',wnames[i]).c_str(), "", N-1, bins );
-  }
-  void operator()(double x) {
-    for (unsigned i=0, n=hh.size(); i<n; ++i)
-      hh[i]->Fill(x,weights[i]);
-  }
-  inline TH1D* operator[](size_t i) const noexcept { return hh[i]; }
-
-  template <typename WK>
-  static void add_all(WK* wk) {
-    for (const hist_bundle* hb : all)
-      for (TH1D* h : hb->hh)
-        wk->addOutput(h);
-  }
-};
-decltype(hist_bundle::wnames) hist_bundle::wnames {
-  "nominal"
-};
-decltype(hist_bundle::weights) hist_bundle::weights;
-decltype(hist_bundle::all) hist_bundle::all;
-
-constexpr double b_pT_yy[] = { 0,20,30,45,60,80,120,170,220,350 };
-
 struct NNLOPSWeights::impl {
-  hist_bundle h_pT_yy;
+  TTree *tree;
 
-  impl()
-  : h_pT_yy("pT_yy",b_pT_yy)
-  {
-    // const double b_pT_yy[] = { 0,20,30,45,60,80,120,170,220,350 };
-    // h_pT_yy = new TH1D("pT_yy","",std::extent<decltype(b_pT_yy)>::value-1,b_pT_yy);
+#ifdef VAR
+#error "Macro name VAR already in use"
+#endif
+#define VAR(NAME) decltype(var::NAME.truth()) NAME;
+  VAR(N_j_30)
+  VAR(pT_yy)
+  VAR(pT_j1_30)
+  VAR(m_jj_30)
+  VAR(Dphi_j_j_30)
+  VAR(Dphi_j_j_30_signed)
+#undef VAR
 
+  xAOD::HiggsWeights hw;
+
+  impl(): tree(new TTree("tree","")) {
+#define VAR(NAME) tree->Branch(#NAME,&NAME);
+  VAR(N_j_30)
+  VAR(pT_yy)
+  VAR(pT_j1_30)
+  VAR(m_jj_30)
+  VAR(Dphi_j_j_30)
+  VAR(Dphi_j_j_30_signed)
+#undef VAR
+    tree->Branch("w_nominal",&hw.nominal);
   }
 };
 
@@ -84,14 +66,13 @@ NNLOPSWeights::~NNLOPSWeights() { }
 
 EL::StatusCode NNLOPSWeights::createOutput() {
   p.reset(new impl);
-  // p = new impl;
   return EL::StatusCode::SUCCESS;
 }
 
 EL::StatusCode NNLOPSWeights::initialize() {
-  const EL::StatusCode code = HgammaAnalysis::initialize();
-  hist_bundle::add_all(wk());
-  return code;
+  const EL::StatusCode status = HgammaAnalysis::initialize();
+  wk()->addOutput(p->tree);
+  return status;
 }
 
 EL::StatusCode NNLOPSWeights::execute() {
@@ -115,22 +96,27 @@ EL::StatusCode NNLOPSWeights::execute() {
     auto jets = truth->applyJetSelection(all_jets);
 
     truth->removeOverlap(photons,jets,electrons,muons);
-    // --------------------------------------------------------------
 
-    // Obtain weights -----------------------------------------------
-    xAOD::HiggsWeights w = event->higgsWeights();
+    HG::VarHandler::getInstance()->setTruthContainers(
+      &all_photons, &electrons, &muons, &jets);
     // --------------------------------------------------------------
 
     // Compute variables --------------------------------------------
-    const TLorentzVector yy = photons[0]->p4() + photons[1]->p4();
+#define VAR(NAME) p->NAME = var::NAME.truth();
+    VAR(N_j_30)
+    VAR(pT_yy)
+    VAR(pT_j1_30)
+    VAR(m_jj_30)
+    VAR(Dphi_j_j_30)
+    VAR(Dphi_j_j_30_signed)
+#undef VAR
     // --------------------------------------------------------------
 
-    hist_bundle::weights[0] = w.nominal;
-
-    // Fill histograms ----------------------------------------------
-    // p->h_pT_yy->Fill(yy.Pt(),w.nominal);
-    p->h_pT_yy(yy.Pt()*1e-3);
+    // Obtain weights -----------------------------------------------
+    p->hw = event->higgsWeights();
     // --------------------------------------------------------------
+
+    p->tree->Fill();
   }
 
   return EL::StatusCode::SUCCESS;
