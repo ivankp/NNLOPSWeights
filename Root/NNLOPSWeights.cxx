37d056374649c4f48da2251a05da9bc58c944aa9
ivankp <ivan.pogrebnyak@gmail.com>
2017-05-17 11:28:58 +0200

    histogram output works

--- Root/NNLOPSWeights.cxx
+++ Root/NNLOPSWeights.cxx
@@ -6,11 +6,23 @@
 
 #include <TLorentzVector.h>
 
-// #include <EventLoop/Worker.h>
-// #include <xAODTruth/TruthEventContainer.h>
 #include <HGamAnalysisFramework/HgammaIncludes.h>
-// #include <TruthWeightTools/HiggsWeightTool.h>
-// #include <HepMC/GenEvent.h>
+
+namespace detail {
+  template <typename S, typename T>
+  inline void cat_impl(S& s, const T& t) { s << t; }
+  template <typename S, typename T, typename... TT>
+  inline void cat_impl(S& s, const T& t, const TT&... tt) {
+    s << t;
+    cat_impl(s,tt...);
+  }
+}
+template <typename... TT>
+inline std::string cat(const TT&... tt) {
+  std::stringstream ss;
+  detail::cat_impl(ss,tt...);
+  return ss.str();
+}
 
 // this is needed to distribute the algorithm to the workers
 ClassImp(NNLOPSWeights)
@@ -18,61 +30,72 @@ ClassImp(NNLOPSWeights)
 using std::cout;
 using std::endl;
 
+class hist_bundle {
+  static constexpr size_t bundle_size = 1;
+  static const std::array<std::string,bundle_size> wnames;
+  static std::vector<const hist_bundle*> all;
+
+  std::array<TH1D*,bundle_size> hh;
+
+public:
+  static std::array<double,bundle_size> weights;
+
+  template <size_t N>
+  hist_bundle(const char* name, const double(&bins)[N]) {
+    all.push_back(this);
+    for (size_t i=0; i<bundle_size; ++i)
+      hh[i] = new TH1D( cat(name,':',wnames[i]).c_str(), "", N-1, bins );
+  }
+  void operator()(double x) {
+    for (unsigned i=0, n=hh.size(); i<n; ++i)
+      hh[i]->Fill(x,weights[i]);
+  }
+  inline TH1D* operator[](size_t i) const noexcept { return hh[i]; }
+
+  template <typename WK>
+  static void add_all(WK* wk) {
+    for (const hist_bundle* hb : all)
+      for (TH1D* h : hb->hh)
+        wk->addOutput(h);
+  }
+};
+decltype(hist_bundle::wnames) hist_bundle::wnames {
+  "nominal"
+};
+decltype(hist_bundle::weights) hist_bundle::weights;
+decltype(hist_bundle::all) hist_bundle::all;
+
+constexpr double b_pT_yy[] = { 0,20,30,45,60,80,120,170,220,350 };
+
 struct NNLOPSWeights::impl {
-
-  // Tools
-  // xAODtoHepMCTool *m_hepmc_tool;
-  // HiggsTruthCategoryTool *m_htxs_tool;
-  // xAOD::HiggsWeightTool *m_higgsMCtool;
-  // xAOD::HiggsWeightTool *weightTool;
-
-  // Histograms
-  TH1 *h_pT_yy;
+  hist_bundle h_pT_yy;
 
   impl()
-  // : m_hepmc_tool(new xAODtoHepMCTool("xAODtoHepMCTool")),
-  //   m_htxs_tool(new HiggsTruthCategoryTool("HiggsTruthCategoryTool")),
-  //   m_higgsMCtool(new xAOD::HiggsWeightTool("HiggsWeightTool"))
-  // : weightTool(new xAOD::HiggsWeightTool("HiggsWeightTool"))
+  : h_pT_yy("pT_yy",b_pT_yy)
   {
-    // weightTool->setProperty("ForceNNLOPS",true);
-    // weightTool->initialize();
+    // const double b_pT_yy[] = { 0,20,30,45,60,80,120,170,220,350 };
+    // h_pT_yy = new TH1D("pT_yy","",std::extent<decltype(b_pT_yy)>::value-1,b_pT_yy);
 
-    const double b_pT_yy[] = { 0,20,30,45,60,80,120,170,220,350 };
-    h_pT_yy = new TH1D("pT_yy","",std::extent<decltype(b_pT_yy)>::value-1,b_pT_yy);
-  }
-  ~impl() {
-    // delete m_higgsMCtool;
-    // delete weightTool;
-    // delete h_pT_yy;
   }
 };
 
 NNLOPSWeights::NNLOPSWeights(const char *name): HgammaAnalysis(name) { }
-NNLOPSWeights::~NNLOPSWeights() {
-  // if (p) {
-  //   delete p;
-  //   p = nullptr;
-  // }
-}
+NNLOPSWeights::~NNLOPSWeights() { }
 
 EL::StatusCode NNLOPSWeights::createOutput() {
-  // p = new impl;
   p.reset(new impl);
+  // p = new impl;
   return EL::StatusCode::SUCCESS;
 }
 
 EL::StatusCode NNLOPSWeights::initialize() {
   const EL::StatusCode code = HgammaAnalysis::initialize();
+  hist_bundle::add_all(wk());
   return code;
 }
 
 EL::StatusCode NNLOPSWeights::execute() {
   HgammaAnalysis::execute();
-
-  // std::vector<std::string> weightNames = p->weightTool->getWeightNames();
-  // for (const auto& name : weightNames)
-  //   cout << name << endl;
 
   auto* event = eventHandler();
   auto* truth = truthHandler();
@@ -96,31 +119,17 @@ EL::StatusCode NNLOPSWeights::execute() {
 
     // Obtain weights -----------------------------------------------
     xAOD::HiggsWeights w = event->higgsWeights();
-    // xAOD::HiggsWeights higgsWeights = truth->weights();
-
-    // const xAOD::TruthEventContainer *truthEvents = nullptr;
-    // const std::vector<float>& weights = truthEvents->at(0)->weights();
-
-    // const xAOD::TruthEventContainer *truthEvents = nullptr;
-    // wk()->xaodEvent()->retrieve(truthEvents,"TruthEvents");
-
-    // std::vector<HepMC::GenEvent> hepmcEvents =
-    //   p->m_hepmc_tool->getHepMCEvents( truthEvents, eventInfo );
-    // HTXS::HiggsClassification htxs =
-    //   p->m_htxs_tool->getHiggsTruthCategoryObject( hepmcEvents[0], m_prodMode );
-    // xAOD::HiggsWeights higgsWeights =
-    //   p->m_higgsMCtool->getHiggsWeights( htxs.jets30.size(), htxs.higgs.Pt() );
-
-    // const xAOD::HiggsWeights w = p->weightTool->getHiggsWeights(
-    //   );
     // --------------------------------------------------------------
 
     // Compute variables --------------------------------------------
     const TLorentzVector yy = photons[0]->p4() + photons[1]->p4();
     // --------------------------------------------------------------
 
+    hist_bundle::weights[0] = w.nominal;
+
     // Fill histograms ----------------------------------------------
-    p->h_pT_yy->Fill(yy.Pt(),w.nominal);
+    // p->h_pT_yy->Fill(yy.Pt(),w.nominal);
+    p->h_pT_yy(yy.Pt()*1e-3);
     // --------------------------------------------------------------
   }
 
