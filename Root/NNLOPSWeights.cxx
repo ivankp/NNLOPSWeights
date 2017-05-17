c557002946813b6379050db6ac7f4421bb3d674b
ivankp <ivan.pogrebnyak@gmail.com>
2017-05-17 17:14:34 +0200

    removed cat (unused)

--- Root/NNLOPSWeights.cxx
+++ Root/NNLOPSWeights.cxx
@@ -9,22 +9,6 @@
 #include <HGamAnalysisFramework/HgammaIncludes.h>
 #include <HGamAnalysisFramework/HGamVariables.h>
 
-namespace detail {
-  template <typename S, typename T>
-  inline void cat_impl(S& s, const T& t) { s << t; }
-  template <typename S, typename T, typename... TT>
-  inline void cat_impl(S& s, const T& t, const TT&... tt) {
-    s << t;
-    cat_impl(s,tt...);
-  }
-}
-template <typename... TT>
-inline std::string cat(const TT&... tt) {
-  std::stringstream ss;
-  detail::cat_impl(ss,tt...);
-  return ss.str();
-}
-
 template <typename T>
 using decay_t = typename std::decay<T>::type;
 
